1e15c8496311e0cb2f56bff55a3d5a8ecc3f7d36
Owen Sowatzke <osowatzke@gmail.com>
2021-03-27 20:35:57 -0400

    Added problem 2.

--- HW4.m
+++ HW4.m
@@ -59,8 +59,77 @@ while (modes_found < 10)
 end
 
 % output table of modes
+fprintf("(1)\n\n");
 T = table(modes',modes_fc','VariableNames',{'mode','fc (GHz)'});
 disp(T);
+
+%% problem 2
+
+% waveguide dimensions in inches
+a = 5.1;
+b = 2.55;
+
+% convert waveguide dimensions to meters
+a = a*0.0254;
+b = b*0.0254;
+
+% determine cutoff wavelength
+Lco = 2*a;
+
+% output cutoff wavelength
+fprintf("2a) Cutoff Wavelength: %.4f m\n", Lco);
+
+% permeability 
+u = 4*pi*1e-7;
+
+% permittivity
+e = 8.854e-12;
+
+% determine cutoff frequency
+fco = 1/(2*a*sqrt(u*e));
+
+% output cutoff frequency
+fprintf("2a) Cutoff Frequency: %.4f GHz\n",fco*1e-9);
+
+% frequency
+f = 10e9;
+
+% determine wavenumber
+k = 2*pi*f*sqrt(u*e);
+
+% determine propagation constant
+B = k*sqrt(1-(fco/f)^2);
+
+% output propation constant
+fprintf("2b) Progation Constant: %.4f rad/m\n",B);
+
+% determine wavelength
+L = (1/sqrt(u*e))/f;
+
+% determine characteristic impedance of dielectric
+n = sqrt(u/e);
+
+% Transverse-wave impedance
+ZTE = n*(1-(L/(2*a))^2)^(-1/2);
+
+% output transverse-wave impedance
+fprintf("2b) Transverse-Wave Impedance: %.4f Ohms\n", ZTE);
+
+% determine TEM propagation constant
+B = k;
+
+% determine TEM transverse wave impedance
+ZTEM = n;
+
+% output propogation constant
+fprintf("2c) TEM Progation Constant: %.4f rad/m\n",B);
+
+% output transverse wave impedance
+fprintf("2c) TEM Transverse-Wave Impedance: %.4f Ohms\n",ZTEM);
+
+%% problem 3
+
+
 
 
 
